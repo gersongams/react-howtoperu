bf198fc161e79c1b49f2883f750362a60e7df723
Gerson Garrido <ggarridom@uni.pe>
2018-02-22 12:53:39 -0500

    first commit

---
+++ client/src/pages/layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+    constructor() {
+      super();
+      console.log("Hi constructor")
+    }
+    render() {
+      return (
+        <div>
+            Hi mom
+        </div>
+      );
+    }
+  }
+  
